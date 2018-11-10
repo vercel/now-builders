7b0f72032e522a235d7c75bd16adf8e9e2b35cc8
Tim Neutkens <timneutkens@me.com>
2018-11-10 14:30:20 +0100

    fix run-user-scripts linting

--- packages/now-build-utils/fs/run-user-scripts.js
+++ packages/now-build-utils/fs/run-user-scripts.js
@@ -2,6 +2,14 @@ const fs = require('fs-extra');
 const path = require('path');
 const { spawn } = require('child_process');
 
+function spawnAsync(command, args, cwd) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, args, { stdio: 'inherit', cwd });
+    child.on('error', reject);
+    child.on('close', (code, signal) => (code !== 0 ? reject(new Error(`Exited with ${code || signal}`)) : resolve()));
+  });
+}
+
 async function runShellScript(fsPath) {
   const destPath = path.dirname(fsPath);
   await spawnAsync(`./${path.basename(fsPath)}`, [], destPath);
@@ -9,29 +17,34 @@ async function runShellScript(fsPath) {
 }
 
 async function shouldUseNpm(destPath) {
+  let currentDestPath = destPath;
+  // eslint-disable-next-line no-constant-condition
   while (true) {
-    if (await fs.exists(path.join(destPath, 'package.json'))) {
-      if (await fs.exists(path.join(destPath, 'package-lock.json'))) {
+    // eslint-disable-next-line no-await-in-loop
+    if (await fs.exists(path.join(currentDestPath, 'package.json'))) {
+      // eslint-disable-next-line no-await-in-loop
+      if (await fs.exists(path.join(currentDestPath, 'package-lock.json'))) {
         return true;
       }
       return false;
     }
 
-    const newDestPath = path.dirname(destPath);
-    if (destPath === newDestPath) break;
-    destPath = newDestPath;
+    const newDestPath = path.dirname(currentDestPath);
+    if (currentDestPath === newDestPath) break;
+    currentDestPath = newDestPath;
   }
 
   return false;
 }
 
 async function runNpmInstall(destPath, args = []) {
+  let commandArgs = args;
   console.log(`installing to ${destPath}`);
   if (await shouldUseNpm(destPath)) {
-    args = args.filter(a => a !== '--prefer-offline');
-    await spawnAsync('npm', ['install'].concat(args), destPath);
+    commandArgs = args.filter(a => a !== '--prefer-offline');
+    await spawnAsync('npm', ['install'].concat(commandArgs), destPath);
   } else {
-    await spawnAsync('yarn', ['--cwd', destPath].concat(args), destPath);
+    await spawnAsync('yarn', ['--cwd', destPath].concat(commandArgs), destPath);
   }
 }
 
@@ -50,17 +63,6 @@ async function runPackageJsonScript(destPath, scriptName) {
   }
 
   return true;
-}
-
-function spawnAsync(command, args, cwd) {
-  return new Promise((resolve, reject) => {
-    const child = spawn(command, args, { stdio: 'inherit', cwd });
-    child.on('error', reject);
-    child.on('close', (code, signal) => {
-      if (code !== 0) return reject(new Error(`Exited with ${code || signal}`));
-      resolve();
-    });
-  });
 }
 
 module.exports = {
