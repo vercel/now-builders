6f69c6ccb2cba32a09b8daadfff63c1d26b8d1d8
Tim Neutkens <timneutkens@me.com>
2018-11-10 13:21:00 +0100

    Fix now/mdx-deck linting

--- packages/now-mdx-deck/index.js
+++ packages/now-mdx-deck/index.js
@@ -12,7 +12,7 @@ const writeFile = promisify(fs.writeFile);
 
 exports.build = async ({ files, entrypoint, workPath }) => {
   console.log('downloading user files...');
-  files = await download(files, workPath);
+  const downloadedFiles = await download(files, workPath);
   console.log('writing package.json...');
   const packageJson = { dependencies: { 'mdx-deck': '1.7.7' } };
   const packageJsonPath = path.join(workPath, 'package.json');
@@ -22,7 +22,7 @@ exports.build = async ({ files, entrypoint, workPath }) => {
   await runNpmInstall(path.dirname(packageJsonPath), ['--prod', '--prefer-offline']);
   console.log('building...');
   const outDir = await getWritableDirectory();
-  const entrypointFsPath = files[entrypoint].fsPath;
+  const entrypointFsPath = downloadedFiles[entrypoint].fsPath;
   const mountpoint = path.dirname(entrypoint);
 
   const build = require(path.join(workPath, 'node_modules/mdx-deck/lib/build.js'));
@@ -36,7 +36,7 @@ exports.build = async ({ files, entrypoint, workPath }) => {
     },
   });
 
-  return await glob('**', outDir, mountpoint);
+  return glob('**', outDir, mountpoint);
 };
 
 exports.prepareCache = async ({ cachePath }) => {
