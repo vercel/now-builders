b3e22c7736a3442b1b4f5d3292dc4c2bbd4706f3
Tim Neutkens <timneutkens@me.com>
2018-11-17 02:18:48 +0100

    Add typescript to now-next

--- packages/now-build-utils/fs/rename.js
+++ packages/now-build-utils/fs/rename.js
@@ -1,6 +1,23 @@
+/** @typedef { import('@now/build-utils/file-ref').Files } Files */
+
+/**
+ * @callback delegate
+ * @argument {string} name
+ * @returns {string}
+ */
+
+/**
+ * Rename files using delegate function
+ * @argument {Files} files
+ * @argument {delegate} delegate
+ * @returns {Files}
+ */
 module.exports = function rename(files, delegate) {
-  return Object.keys(files).reduce((newFiles, name) => ({
-    ...newFiles,
-    [delegate(name)]: files[name],
-  }), {});
+  return Object.keys(files).reduce(
+    (newFiles, name) => ({
+      ...newFiles,
+      [delegate(name)]: files[name],
+    }),
+    {},
+  );
 };
