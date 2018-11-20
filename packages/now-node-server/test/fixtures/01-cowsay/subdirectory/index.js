edf9c61ae241a250b82b8f72f04876d430254518
Igor Klopov <igor@klopov.com>
2018-11-20 19:14:49 +0300

    test for process.env.NODE_ENV replacement

--- packages/now-node-server/test/fixtures/01-cowsay/subdirectory/index.js
+++ packages/now-node-server/test/fixtures/01-cowsay/subdirectory/index.js
@@ -1,6 +1,9 @@
 const yodasay = require('yodasay/build/yodasay.umd.js').say;
 const http = require('http');
 
+// test that process.env is not replaced by webpack
+process.env.NODE_ENV = 'development';
+
 const server = http.createServer((req, resp) => {
   resp.end(yodasay({ text: 'yoda:RANDOMNESS_PLACEHOLDER' }));
 });
