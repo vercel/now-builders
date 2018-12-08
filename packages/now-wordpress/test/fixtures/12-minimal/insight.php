8f4acec949cb0dd362ca91de0aa852b578356bb9
Igor Klopov <igor@klopov.com>
2018-12-08 09:10:40 +0300

    update now.json

--- packages/now-wordpress/test/fixtures/12-minimal/insight.php
+++ packages/now-wordpress/test/fixtures/12-minimal/insight.php
@@ -15,3 +15,6 @@ print('site_url: ' . site_url() . PHP_EOL);
 print('home_url: ' . home_url() . PHP_EOL);
 print('content_url: ' . content_url() . PHP_EOL);
 print('WP_CONTENT_URL: ' . WP_CONTENT_URL . PHP_EOL);
+print('LOGGED_IN_COOKIE: ' . LOGGED_IN_COOKIE . PHP_EOL);
+print('$_COOKIE[LOGGED_IN_COOKIE]: ' . $_COOKIE[LOGGED_IN_COOKIE] . PHP_EOL);
+var_dump(wp_validate_auth_cookie());
