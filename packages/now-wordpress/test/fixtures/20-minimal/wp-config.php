8fcc1eb250947124cf4d52557df0cfa70e6bd48e
Igor Klopov <igor@klopov.com>
2018-12-18 09:20:41 +0300

    update they way secrets are passed

--- packages/now-wordpress/test/fixtures/20-minimal/wp-config.php
+++ packages/now-wordpress/test/fixtures/20-minimal/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', '');
+define('DB_NAME', $_ENV['DB_NAME']);
 
 /** MySQL database username */
-define('DB_USER', '');
+define('DB_USER', $_ENV['DB_USER']);
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
 
 /** MySQL hostname */
-define('DB_HOST', '');
+define('DB_HOST', $_ENV['DB_HOST']);
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
