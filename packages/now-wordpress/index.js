0993bffb7207ffb2563f870458691e0c19c46352
Igor Klopov <igor@klopov.com>
2018-12-18 05:48:16 +0300

    download, decompress and merge release url

--- packages/now-wordpress/index.js
+++ packages/now-wordpress/index.js
@@ -1,15 +1,81 @@
+const assert = require('assert');
 const { createLambda } = require('@now/build-utils/lambda.js');
+const fetch = require('node-fetch');
+const FileBlob = require('@now/build-utils/file-blob.js');
+const { getFiles } = require('@now/php-bridge');
 const path = require('path');
 const rename = require('@now/build-utils/fs/rename.js');
-const { getFiles } = require('@now/php-bridge');
+const streamToBuffer = require('@now/build-utils/fs/stream-to-buffer.js');
+const yauzl = require('yauzl');
 
 exports.config = {
   maxLambdaSize: '20mb',
 };
 
-exports.build = async ({ files, entrypoint }) => {
-  // move all user code to 'user' subdirectory
-  const userFiles = rename(files, name => path.join('user', name));
+async function readReleaseUrl(releaseUrl) {
+  const resp = await fetch(releaseUrl);
+
+  if (!resp.ok) {
+    throw new Error(`Failed to download ${releaseUrl}. Status code is ${resp.status}`);
+  }
+
+  return resp.buffer();
+}
+
+const prefixRegexp = /^wordpress\//;
+
+function decompressBuffer(buffer) {
+  return new Promise((resolve, reject) => {
+    const files = {};
+
+    yauzl.fromBuffer(buffer, { lazyEntries: true }, (error, zipfile) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      zipfile.readEntry();
+      zipfile.on('entry', (entry) => {
+        const { fileName } = entry;
+
+        if (/\/$/.test(fileName)) {
+          zipfile.readEntry();
+          return;
+        }
+
+        zipfile.openReadStream(entry, (error2, readStream) => {
+          if (error2) {
+            reject(error2);
+            return;
+          }
+
+          streamToBuffer(readStream).then((data) => {
+            assert(prefixRegexp.test(fileName), fileName);
+            const fileName2 = fileName.replace(prefixRegexp, '');
+            files[fileName2] = new FileBlob({ data });
+            zipfile.readEntry();
+          }).catch(reject);
+        });
+      });
+
+      zipfile.on('end', () => resolve(files));
+    });
+  });
+}
+
+exports.build = async ({ files, entrypoint, config }) => {
+  if (!config.releaseUrl) {
+    throw new Error('Config must contain a "releaseUrl" for wordpress.zip');
+  }
+
+  console.log('downloading release url...');
+  const releaseBuffer = await readReleaseUrl(config.releaseUrl);
+  console.log('decompressing release url...');
+  const releaseFiles = await decompressBuffer(releaseBuffer);
+  const mergedFiles = { ...releaseFiles, ...files };
+
+  // move all code to 'user' subdirectory
+  const userFiles = rename(mergedFiles, name => path.join('user', name));
   const bridgeFiles = await getFiles();
 
   const lambda = await createLambda({
