768ef90e143212bc526833c94fd495e398ff9dd7
Sebastian Ferrari <sebas5384@gmail.com>
2019-01-09 21:41:36 -0200

    Added php-cli and refactored the build of the now-php-bridge.

--- packages/now-php-bridge/build/build.sh
+++ packages/now-php-bridge/build/build.sh
@@ -1,17 +1,15 @@
-rm -rf ../native
-mkdir -p ../native/modules
 docker rmi now-php-docker-image --force
-docker build . -t now-php-docker-image
-docker run now-php-docker-image
-docker run now-php-docker-image /bin/cat /usr/bin/php > ../native/php
-docker run now-php-docker-image /bin/cat /usr/sbin/php-fpm > ../native/php-fpm
-docker run now-php-docker-image /bin/cat /root/app/php.ini > ../native/php.ini
-docker run now-php-docker-image /bin/cat /root/app/php-fpm.ini > ../native/php-fpm.ini
-docker run now-php-docker-image /bin/cat /usr/lib64/php/modules/curl.so > ../native/modules/curl.so
-docker run now-php-docker-image /bin/cat /usr/lib64/php/modules/json.so > ../native/modules/json.so
-docker run now-php-docker-image /bin/cat /usr/lib64/php/modules/mbstring.so > ../native/modules/mbstring.so
-docker run now-php-docker-image /bin/cat /usr/lib64/php/modules/mysqli.so > ../native/modules/mysqli.so
-docker run now-php-docker-image /bin/cat /usr/lib64/mysql/libmysqlclient.so.16 > ../native/modules/libmysqlclient.so.16
-docker run now-php-docker-image /bin/cat /usr/lib64/php/modules/opcache.so > ../native/modules/opcache.so
+docker build --no-cache . -t now-php-docker-image
+
+# Build and test container.
+docker run --name now-php-build now-php-docker-image 
+
+# Extract native files.
+rm -rf ../native
+docker cp now-php-build:/root/app/native ../
+
+# Remove php build.
+docker rm -f now-php-build
+
 chmod +x ../native/php
 chmod +x ../native/php-fpm
--- packages/now-php-bridge/build/test.sh
+++ packages/now-php-bridge/build/test.sh
@@ -1,18 +1,33 @@
-mkdir -p /root/app/modules
-cp /usr/bin/php /root/app/php
-cp /usr/sbin/php-fpm /root/app/php-fpm
-cp /usr/lib64/php/modules/curl.so /root/app/modules/curl.so
-cp /usr/lib64/php/modules/json.so /root/app/modules/json.so
-cp /usr/lib64/php/modules/mbstring.so /root/app/modules/mbstring.so
-cp /usr/lib64/php/modules/mysqli.so /root/app/modules/mysqli.so
-cp /usr/lib64/mysql/libmysqlclient.so.16 /root/app/modules/libmysqlclient.so.16
-cp /usr/lib64/php/modules/opcache.so /root/app/modules/opcache.so
+# PHP binaries.
+cp /usr/bin/php /root/app/native/php
+cp /usr/sbin/php-fpm /root/app/native/php-fpm
+
+# PHP Modules.
+cp -r /usr/lib64/php/modules /root/app/native/
+
+# Shared lib missing on Linux's lambda.
+cp /usr/lib64/mysql/libmysqlclient.so.16 /root/app/native/modules/libmysqlclient.so.16
+cp /usr/lib64/libedit.so.0 /root/app/native/modules/libedit.so.0
+cp /usr/lib64/libXpm.so.4 /root/app/native/modules/libXpm.so.4
+
+patchelf --set-rpath '$ORIGIN:$ORIGIN/modules' /root/app/native/php
+patchelf --set-rpath '$ORIGIN:$ORIGIN/modules' /root/app/native/php-fpm
+patchelf --set-rpath '$ORIGIN:$ORIGIN/modules' /root/app/native/modules/mysqli.so
+patchelf --set-rpath '$ORIGIN:$ORIGIN/modules' /root/app/native/modules/pdo_mysql.so
+patchelf --set-rpath '$ORIGIN:$ORIGIN/modules' /root/app/native/modules/gd.so
+patchelf --set-rpath '$ORIGIN:$ORIGIN/modules' /root/app/native/modules/xmlreader.so
+
+# Simulates a clean environment.
 rm -rf $(which php)
 rm -rf $(which php-fpm)
+rm -f /usr/lib64/libedit.so.0
 rm -rf /usr/lib64/php
 rm -rf /usr/lib64/mysql
 rm -rf /etc/php.d
-./php-fpm --help
-./php -c php.ini test.php
+
+# Test binaries.
+./native/php-fpm --help
+./native/php -c ./native/php.ini test.php
+
 echo "if you see 'can't connect to local mysql' - it is good - mysql library is found and used"
 echo "if you see 'call to undefined function mysqli_connect' - it is bad - something went wrong"
