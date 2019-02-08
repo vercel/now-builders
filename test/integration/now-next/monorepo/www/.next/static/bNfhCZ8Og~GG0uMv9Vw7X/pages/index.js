fd78fca22279dab84bcc1c1ecc19aad6073f8d82
Connor Davis <mail@connordav.is>
2019-02-07 22:48:12 -0600

    Download whole directory

---
+++ test/integration/now-next/monorepo/www/.next/static/bNfhCZ8Og~GG0uMv9Vw7X/pages/index.js
@@ -0,0 +1,27 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [4],
+  {
+    RNiq: function(n, o, e) {
+      'use strict';
+      e.r(o);
+      var t = e('wZ9u'),
+        u = e.n(t);
+      o.default = function() {
+        return ''.concat(u()(), ' Welcome to the index page');
+      };
+    },
+    vlRD: function(n, o, e) {
+      (window.__NEXT_P = window.__NEXT_P || []).push([
+        '/',
+        function() {
+          var n = e('RNiq');
+          return { page: n.default || n };
+        }
+      ]);
+    },
+    wZ9u: function(n, o) {
+      n.exports = () => 'Hello!';
+    }
+  },
+  [['vlRD', 0]]
+]);
