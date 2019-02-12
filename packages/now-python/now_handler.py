073bcc1d0ad930d8853e5e3f50df39e359a80200
Nathaniel Hill <nata@goguna.com>
2019-02-12 11:09:44 -0500

    support base64 encoded request body

--- packages/now-python/now_handler.py
+++ packages/now-python/now_handler.py
@@ -12,14 +12,20 @@ def now_handler(event, context):
     path = payload['path']
     headers = payload['headers']
     method = payload['method']
-    data = payload['body']
-    
+    encoding = payload.get("encoding")
+    body = payload['body']
+
+    if (
+        (body is not None and len(body) > 0) and
+        (encoding is not None and encoding == "base64")
+    ):
+        body = base64.b64decode(body)
+
     res = requests.request(method, 'http://0.0.0.0:3000' + path,
-            headers=headers, data=data)
+            headers=headers, data=body)
 
     return {
         'statusCode': res.status_code,
         'headers': dict(res.headers),
         'body': res.text
     }
-
