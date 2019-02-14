86819bb6c9e584e657d05fae8ea24bf51e0582a9
Nathan Rajlich <nathan@tootallnate.net>
2019-02-14 14:17:03 -0800

    Tweak comments

--- packages/now-go/util/install.js
+++ packages/now-go/util/install.js
@@ -2,13 +2,13 @@ const { join } = require('path');
 const { downloadGo } = require('../go-helpers');
 
 async function main() {
-  // First download the `go` binary for this platform/arch
+  // First download the `go` binary for this platform/arch.
   const go = await downloadGo();
 
-  // Build the `get-exported-function-name` helper program
+  // Build the `get-exported-function-name` helper program.
+  // `go get` is not necessary because the program has no external deps.
   const src = join(__dirname, 'get-exported-function-name.go');
   const dest = join(__dirname, '../get-exported-function-name');
-  // No `go get` is not necessary because the program has no external deps
   await go.build({ src, dest });
 }
 
