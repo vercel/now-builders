9006346b1db7b9090227714e5ef0531b7087c023
Joe Haddad <joe.haddad@zeit.co>
2019-02-27 16:00:12 -0500

    Check if path exists instead of globbing

--- packages/now-next/index.js
+++ packages/now-next/index.js
@@ -15,6 +15,7 @@ const {
   remove: removePath,
   mkdirp,
   rename: renamePath,
+  pathExists,
 } = require('fs-extra');
 const semver = require('semver');
 const nextLegacyVersions = require('./legacy-versions');
@@ -122,8 +123,7 @@ exports.build = async ({ files, workPath, entrypoint }) => {
   await download(files, workPath);
   const entryPath = path.join(workPath, entryDirectory);
 
-  const preflightDotNextFiles = await glob('.next/**', entryPath);
-  if (Object.keys(preflightDotNextFiles).length > 0) {
+  if (await pathExists(path.join(entryPath, '.next'))) {
     console.warn(
       'WARNING: You should probably not upload the `.next` directory. See https://zeit.co/docs/v2/deployments/official-builders/next-js-now-next/ for more information.',
     );
