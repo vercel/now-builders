8f3e7045a1a4b0808ffbaa1eaa86e218eb473922
Ehbraheem <ehbraheem@gmail.com>
2019-03-08 05:39:46 +1100

    Redefine 'now_handler' method to support user-defined handler

--- packages/now-ruby/now_handler.rb
+++ packages/now-ruby/now_handler.rb
@@ -3,27 +3,32 @@ require 'json'
 
 require_relative _NOW_HANDLER_FILE
 
+module NowHandler
+  # server = WEBrick::HTTPServer.new :Port => 3000
+  # server.mount '/', Handler
+  # trap('INT') { server.stop }
+  # server.start
 
-server = WEBrick::HTTPServer.new :Port => 3000
-server.mount '/', HANDLER
-trap('INT') { server.stop }
-server.start
 
+  def self.now_handler(event:, context:)
+    server = WEBrick::HTTPServer.new :Port => 3000
+    Thread.new(server) do |server|
+      if Object.const_defined?('Handler')
+        server.mount '/', Handler
+      else
+        server.mount_proc "/" do |req, res|
+        payload = JSON.parse event['body']
+        path = payload['path']
+        headers = payload['headers']
+        http_method = payload['method']
 
-def now_handler(event, context)
-  server = WEBrick::HTTPServer.new :Port => 3000
-  Thread.new(server) do |server|
-    server.mount_proc "/" do |req, res|
-      payload = JSON.parse event['body']
-      path = payload['path']
-      headers = payload['headers']
-      http_method = payload['method']
-
-      res.body = "#{http_method}, 'http://0.0.0.0:3000'#{path} #{headers}"
-      res.status = 200
-      res.headers = headers
+        res.body = "#{http_method}, 'http://0.0.0.0:3000'#{path} #{headers}"
+        res.status = 200
+        res.headers = headers
+      end
+      end
     end
+    trap('INT') { server.stop }
+    server.start
   end
-  trap('INT') { server.stop }
-  server.start
 end
