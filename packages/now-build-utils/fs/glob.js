809fca4e6a767d7427836337ee3c3745af673e06
Nathan Rajlich <nathan@tootallnate.net>
2019-03-08 14:13:01 -0800

    Use `glob-gitignore` instead of vanilla `glob`
    See: https://www.npmjs.com/package/glob-gitignore
    
    This module merges `glob` and the `ignore` packages for performance
    purposes. This is mainly for `now dev` (see zeit/now-cli#1899), so
    that we can easily pass the `Ignore` instance to the glob function
    to avoid unnecessary directory traversals.

--- packages/now-build-utils/fs/glob.js
+++ packages/now-build-utils/fs/glob.js
@@ -1,6 +1,6 @@
-const assert = require('assert');
 const path = require('path');
-const vanillaGlob = require('glob');
+const assert = require('assert');
+const { glob: vanillaGlob } = require('glob-gitignore');
 const FileFsRef = require('../file-fs-ref.js');
 
 /** @typedef {import('fs').Stats} Stats */
@@ -13,55 +13,49 @@ const FileFsRef = require('../file-fs-ref.js');
  * @argument {string} [mountpoint]
  * @returns {Promise<GlobFiles>}
  */
-module.exports = function glob(pattern, opts = {}, mountpoint) {
-  return new Promise((resolve, reject) => {
-    /** @type {GlobOptions} */
-    let options;
-    if (typeof opts === 'string') {
-      options = { cwd: opts };
-    } else {
-      options = opts;
+module.exports = async function glob(pattern, opts = {}, mountpoint) {
+  /** @type {GlobOptions} */
+  let options;
+  if (typeof opts === 'string') {
+    options = { cwd: opts };
+  } else {
+    options = opts;
+  }
+
+  if (!options.cwd) {
+    throw new Error(
+      'Second argument (basePath) must be specified for names of resulting files',
+    );
+  }
+
+  if (!path.isAbsolute(options.cwd)) {
+    throw new Error(`basePath/cwd must be an absolute path (${options.cwd})`);
+  }
+
+  options.statCache = {};
+  options.stat = true;
+  options.dot = true;
+
+  // eslint-disable-next-line consistent-return
+  const files = await vanillaGlob(pattern, options);
+
+  return files.reduce((files2, relativePath) => {
+    const fsPath = path.join(options.cwd, relativePath);
+    /** @type {Stats|any} */
+    const stat = options.statCache[fsPath];
+    assert(
+      stat,
+      `statCache does not contain value for ${relativePath} (resolved to ${fsPath})`,
+    );
+    if (stat && stat.isFile()) {
+      let finalPath = relativePath;
+      if (mountpoint) finalPath = path.join(mountpoint, finalPath);
+      return {
+        ...files2,
+        [finalPath]: new FileFsRef({ mode: stat.mode, fsPath }),
+      };
     }
 
-    if (!options.cwd) {
-      throw new Error(
-        'Second argument (basePath) must be specified for names of resulting files',
-      );
-    }
-
-    if (!path.isAbsolute(options.cwd)) {
-      throw new Error(`basePath/cwd must be an absolute path (${options.cwd})`);
-    }
-
-    options.statCache = {};
-    options.stat = true;
-    options.dot = true;
-
-    // eslint-disable-next-line consistent-return
-    vanillaGlob(pattern, options, (error, files) => {
-      if (error) return reject(error);
-
-      resolve(
-        files.reduce((files2, relativePath) => {
-          const fsPath = path.join(options.cwd, relativePath);
-          /** @type {Stats|any} */
-          const stat = options.statCache[fsPath];
-          assert(
-            stat,
-            `statCache does not contain value for ${relativePath} (resolved to ${fsPath})`,
-          );
-          if (stat && stat.isFile()) {
-            let finalPath = relativePath;
-            if (mountpoint) finalPath = path.join(mountpoint, finalPath);
-            return {
-              ...files2,
-              [finalPath]: new FileFsRef({ mode: stat.mode, fsPath }),
-            };
-          }
-
-          return files2;
-        }, {}),
-      );
-    });
-  });
+    return files2;
+  }, {});
 };
