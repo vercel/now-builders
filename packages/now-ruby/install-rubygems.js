bce33378e9a4762c7942f4bf849989eaf0a6d470
Ehbraheem <ehbraheem@gmail.com>
2019-03-09 11:00:10 +1100

    Added Bundler configurations and production specific behaviors

--- packages/now-ruby/install-rubygems.js
+++ packages/now-ruby/install-rubygems.js
@@ -1,13 +1,23 @@
 const cli = require('./cli');
 
-module.exports = async (gemfilePath, ...args) => {
+const bundle = async command => async (...options) => {
+  await cli('bundle', command, ...options);
+};
+
+const config = bundle('config');
+
+module.exports = async (gemfilePath, srcPath, ...args) => {
+  // process.chdir(srcPath)
   console.log('Installing your dependencies.....');
   await cli('gem', 'i', 'bundler');
-  await cli('bundle', 'i', `--gemfile=${gemfilePath}`, ...args); // , '--deployment' )
+  await config('without', 'development:test:ci');
+  await config('auto_install', 'true');
+  await bundle('i')(`--gemfile=${gemfilePath}`, ...args); // , '--deployment' )
+  await bundle('package', '--all');
   console.log('Dependencies installed correctly.....');
 
   return new Promise((resolve, reject) => {
-    cli('bundle', 'show')
+    bundle('show')()
       .on('error', reject)
       .on('finish', result => console.log(result.stdout));
   });
