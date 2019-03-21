e4dc63d7f54ea53252966fa35efc67f207593e72
Steven <steven@ceriously.com>
2019-03-21 16:01:02 -0400

    Add missing fs

--- packages/now-build-utils/src/index.ts
+++ packages/now-build-utils/src/index.ts
@@ -3,6 +3,12 @@ import FileFsRef from './file-fs-ref';
 import FileRef from './file-ref';
 import { File, Files } from './types';
 import { Lambda, createLambda } from './lambda';
+import download from './fs/download';
+import getWriteableDirectory from './fs/get-writable-directory'
+import glob from './fs/glob';
+import rename from './fs/rename';
+import { installDependencies, runPackageJsonScript, runNpmInstall, runShellScript } from './fs/run-user-scripts';
+import streamToBuffer from './fs/stream-to-buffer';
 
 export {
     FileBlob,
@@ -12,4 +18,11 @@ export {
     File,
     Lambda,
     createLambda,
+    download,
+    getWriteableDirectory,
+    glob,
+    rename,
+    installDependencies, runPackageJsonScript, runNpmInstall, runShellScript,
+    streamToBuffer
 };
+
