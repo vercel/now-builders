28f6450497c68c1f4931324f00181f96c12b666b
Ties <1744962+ties-v@users.noreply.github.com>
2019-03-26 23:44:59 +0100

    [@now/php] Added includeFiles config to specify the exact files that are available for the PHP lambda.
    This is a retry of https://github.com/zeit/now-builders/pull/37, where the git history is a mess.

--- packages/now-php/index.js
+++ packages/now-php/index.js
@@ -2,14 +2,29 @@ const { createLambda } = require('@now/build-utils/lambda.js'); // eslint-disabl
 const path = require('path');
 const rename = require('@now/build-utils/fs/rename.js'); // eslint-disable-line import/no-extraneous-dependencies
 const { getFiles } = require('@now/php-bridge');
+const minimatch = require('minimatch');
 
 exports.config = {
   maxLambdaSize: '10mb',
 };
 
-exports.build = async ({ files, entrypoint }) => {
+exports.build = async ({ files, entrypoint, config }) => {
+  // Fetch the included files config, or default (**)
+  const includedFilesGlob = (config ? config.includeFiles : false) || '**';
+  let includedFiles = files;
+  if (includedFilesGlob !== '**') {
+    // match the files with the glob
+    includedFiles = Object.keys(files)
+      .filter(minimatch.filter(includedFilesGlob))
+      .reduce((res, key) => {
+        res[key] = files[key];
+        return res;
+      }, {});
+    // explicit and always include the entrypoint
+    includedFiles[entrypoint] = files[entrypoint];
+  }
   // move all user code to 'user' subdirectory
-  const userFiles = rename(files, name => path.join('user', name));
+  const userFiles = rename(includedFiles, name => path.join('user', name));
   const bridgeFiles = await getFiles();
 
   // TODO config.extensions. OR php.ini from user
