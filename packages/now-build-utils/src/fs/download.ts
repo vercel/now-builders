fbf543cab1b12148d0bf95e5467b1d1bc3aa8edf
James Homer <jameshomer85@gmail.com>
2019-04-03 18:03:02 +0100

    Download files sequentially in chunks

--- packages/now-build-utils/src/fs/download.ts
+++ packages/now-build-utils/src/fs/download.ts
@@ -6,6 +6,20 @@ export interface DownloadedFiles {
   [filePath: string]: FileFsRef
 }
 
+const CHUNK_SIZE = 50;
+
+function chunkArray(array: Array<any>, chunkSize: number){
+  const chunks = [];
+  while (array.length) {
+    chunks.push(array.splice(0, chunkSize));
+  }
+  return chunks;
+}
+
+function inSequence(tasks: Array<any>) {
+  return tasks.reduce((promise, task) => promise.then(task), Promise.resolve())
+}
+
 async function downloadFile(file: File, fsPath: string): Promise<FileFsRef> {
   const { mode } = file;
   const stream = file.toStream();
@@ -15,13 +29,19 @@ async function downloadFile(file: File, fsPath: string): Promise<FileFsRef> {
 export default async function download(files: Files, basePath: string): Promise<DownloadedFiles> {
   const files2: DownloadedFiles = {};
 
-  await Promise.all(
-    Object.keys(files).map(async (name) => {
-      const file = files[name];
-      const fsPath = path.join(basePath, name);
-      files2[name] = await downloadFile(file, fsPath);
-    }),
-  );
+  const filenamesChunks = chunkArray(Object.keys(files), CHUNK_SIZE);
+
+  const tasks = filenamesChunks.map(filenames => () => {
+    return Promise.all(
+      filenames.map(async (name) => {
+        const file = files[name];
+        const fsPath = path.join(basePath, name);
+        files2[name] = await downloadFile(file, fsPath);
+      }),
+    );
+  })
+
+  await inSequence(tasks);
 
   return files2;
 };
