9c4db435d8a0d325cdfc80b5332f4ebd6e4b9810
Contextualist <harzjc@gmail.com>
2019-04-03 11:04:42 +0700

    Update install flag `--upgrade` in the right order.
    Co-Authored-By: sophearak <t.sophearak@gmail.com>

--- packages/now-python/index.js
+++ packages/now-python/index.js
@@ -12,7 +12,7 @@ const downloadAndInstallPip = require('./download-and-install-pip');
 async function pipInstall(pipPath, workDir, ...args) {
   console.log(`running "pip install --target ${workDir} ${args.join(' ')}"...`);
   try {
-    await execa(pipPath, ['install', '--target', '.', ...args], {
+    await execa(pipPath, ['install', '--target', '.', '--upgrade', ...args], {
       cwd: workDir,
       stdio: 'inherit',
     });
