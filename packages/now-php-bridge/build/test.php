2e7f3b584793641ee1a7c4acc1a96a1877b77831
James Homer <jameshomer85@gmail.com>
2019-04-04 01:07:35 +0100

    add tests

--- packages/now-php-bridge/build/test.php
+++ packages/now-php-bridge/build/test.php
@@ -2,3 +2,5 @@
 mysqli_connect();
 print('php_sapi_name=' . php_sapi_name() . PHP_EOL);
 print('opcache_enabled=' . opcache_get_status()['opcache_enabled'] . PHP_EOL);
+print('simplexml_loaded=' . extension_loaded('simplexml') . PHP_EOL);
+print('xmlwriter_loaded=' . extension_loaded('xmlwriter') . PHP_EOL);
