e56afa8947b8a638306bfae339ac4f81af30f792
Steven <steven@ceriously.com>
2019-04-14 18:36:57 -0400

    Fix line numbers

--- packages/now-node/test/fixtures/13-stack-trace/index.js
+++ packages/now-node/test/fixtures/13-stack-trace/index.js
@@ -1,13 +1,9 @@
 module.exports = (req, res) => {
   try {
     if (req) {
-      throw new Error(
-        `You should see this error ${process.env.RANDOMNESS_ENV_VAR}`,
-      );
+      throw new Error(`Should throw ${process.env.RANDOMNESS_ENV_VAR}`);
     }
-    res.end(
-      `You should not see this message ${process.env.RANDOMNESS_ENV_VAR}`,
-    );
+    res.end(`Should not print ${process.env.RANDOMNESS_ENV_VAR}`);
   } catch (error) {
     res.end(error.stack);
   }
