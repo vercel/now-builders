05a1476dc7ee7db6d9c12cef15e8aa554221d032
Mark Pena <markdrew53@gmail.com>
2019-04-15 14:27:23 -0500

    adding types

--- packages/now-optipng/src/index.ts
+++ packages/now-optipng/src/index.ts
@@ -1,9 +1,13 @@
 // eslint-disable-line import/no-extraneous-dependencies
-import { FileBlob, BuildOptions } from '@now/build-utils'
+import { 
+  FileBlob, 
+  BuildOptions,
+  AnalyzeOptions
+} from '@now/build-utils'
 import OptiPng from 'optipng'
 import pipe from 'multipipe'
 
-export function analyze({ files, entrypoint }) {
+export function analyze({ files, entrypoint }: AnalyzeOptions) {
   return files[entrypoint].digest
 }
 
