7415d33857b68bc5109371652c5b97eeb402a202
Mark Pena <markdrew53@gmail.com>
2019-04-17 21:50:37 -0500

    adding types

--- packages/now-optipng/src/index.ts
+++ packages/now-optipng/src/index.ts
@@ -15,7 +15,7 @@ export async function build({ files, entrypoint }: BuildOptions) {
   const optimizer = new OptiPng(['-o9']);
   const stream = pipe(
     files[entrypoint].toStream(),
-    optimizer,
+    optimizer
   );
   const result = await FileBlob.fromStream({ stream });
   return { [entrypoint]: result };
