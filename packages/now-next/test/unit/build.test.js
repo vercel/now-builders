c2862698fdd41509b4e3e6309f5baaa064bfe016
Steven <steven@ceriously.com>
2019-04-23 16:03:07 -0400

    Add builder v2 tests

--- packages/now-next/test/unit/build.test.js
+++ packages/now-next/test/unit/build.test.js
@@ -4,7 +4,7 @@ const os = require('os');
 const { build } = require('@now/next');
 const { FileBlob } = require('@now/build-utils');
 
-jest.setTimeout(20000);
+jest.setTimeout(45000);
 
 describe('build meta dev', () => {
   const files = {
@@ -46,8 +46,8 @@ describe('build meta dev', () => {
       .slice(3),
   );
   console.log('workPath directory: ', workPath);
-
-  it('should have builder v2 response', async () => {
+  /*
+  it('should have builder v2 response isDev=false', async () => {
     const meta = { isDev: false, requestPath: null };
     const { output, routes, watch } = await build({
       files,
@@ -55,10 +55,31 @@ describe('build meta dev', () => {
       entrypoint,
       meta,
     });
-    console.log('output: ', Object.keys(output));
-    expect(Object.keys(output).length).toBe(3);
+    //console.log('output: ', Object.keys(output));
+    expect(Object.keys(output).length).toBe(7);
     expect(output.index.type).toBe('Lambda');
     expect(routes.length).toBe(0);
     expect(watch.length).toBe(0);
+  });
+  */
+
+  it('should have builder v2 response isDev=true', async () => {
+    const meta = { isDev: true, requestPath: null };
+    const { output, routes, watch } = await build({
+      files,
+      workPath,
+      entrypoint,
+      meta,
+    });
+    // console.log('routes: ', routes);
+    // console.log('watch: ', watch);
+    expect(output).toEqual({});
+    expect(routes).toEqual([
+      { src: '/_next/(.*)', dest: 'http://localhost:5000/_next/$1' },
+      { src: '/static/(.*)', dest: 'http://localhost:5000/static/$1' },
+      { src: '/index', dest: 'http://localhost:5000/index' },
+      { src: '/', dest: 'http://localhost:5000/' },
+    ]);
+    expect(watch).toEqual(['next.config.js', 'pages/index.js', 'package.json']);
   });
 });
