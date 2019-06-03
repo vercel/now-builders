6d81e6599284743a5dca03a8188d17226abd44ee
Steven <steven@ceriously.com>
2019-06-03 17:36:50 -0400

    Add now-layer-yarn

---
+++ packages/now-layer-yarn/src/index.ts
@@ -0,0 +1,32 @@
+import { tmpdir } from 'os';
+import { join } from 'path';
+import { glob } from '@now/build-utils';
+import { mkdir, remove, pathExists } from 'fs-extra';
+import { install } from './install';
+
+interface LayerConfig {
+	runtimeVersion: string;
+	platform: string;
+	arch: string;
+}
+
+export async function buildLayer({
+	runtimeVersion,
+	platform,
+	arch,
+}: LayerConfig) {
+	const dir = join(
+		tmpdir(),
+		`now-layer-yarn-${runtimeVersion}-${platform}-${arch}`
+	);
+	const exists = await pathExists(dir);
+	if (exists) {
+		await remove(dir);
+	}
+	await mkdir(dir);
+	await install(dir, runtimeVersion);
+	const files = await glob('{bin/**,lib/**}', {
+		cwd: dir,
+	});
+	return { files };
+}
--- packages/now-layer-yarn/test/test.js
+++ packages/now-layer-yarn/test/test.js
@@ -3,45 +3,44 @@ jest.setTimeout(30 * 1000);
 const { buildLayer } = require('../');
 
 describe('buildLayer', () => {
-  it('should get npm 6 but not npm for windows', async () => {
+  it('should get yarn for windows', async () => {
     const { files } = await buildLayer({
-      runtimeVersion: '6.9.0',
+      runtimeVersion: '1.16.0',
       platform: 'win32',
       arch: 'x64',
     });
     const names = new Set(Object.keys(files));
     expect(names).toBeTruthy();
     expect(names.size).toBeGreaterThan(0);
-    expect(names.has('bin/npm.cmd')).toBeTruthy();
-    expect(names.has('bin/npx.cmd')).toBeTruthy();
-    expect(names.has('README.md')).toBeFalsy();
+    expect(names.has('bin/yarn.cmd')).toBeTruthy();
+    expect(names.has('lib/cli.js')).toBeTruthy();
   });
 
-  it('should get npm 6 but not npm for macos', async () => {
+  it('should get yarn for macos', async () => {
     const { files } = await buildLayer({
-      runtimeVersion: '6.9.0',
+      runtimeVersion: '1.16.0',
       platform: 'darwin',
       arch: 'x64',
     });
     const names = new Set(Object.keys(files));
     expect(names).toBeTruthy();
     expect(names.size).toBeGreaterThan(0);
-    expect(names.has('bin/npm')).toBeTruthy();
-    expect(names.has('bin/npx')).toBeTruthy();
+    expect(names.has('bin/yarn')).toBeTruthy();
+    expect(names.has('lib/cli.js')).toBeTruthy();
     expect(names.has('README.md')).toBeFalsy();
   });
 
-  it('should get npm 6 but not npm for linux', async () => {
+  it('should get yarn for linux', async () => {
     const { files } = await buildLayer({
-      runtimeVersion: '6.9.0',
+      runtimeVersion: '1.16.0',
       platform: 'linux',
       arch: 'x64',
     });
     const names = new Set(Object.keys(files));
     expect(names).toBeTruthy();
     expect(names.size).toBeGreaterThan(0);
-    expect(names.has('bin/npm')).toBeTruthy();
-    expect(names.has('bin/npx')).toBeTruthy();
+    expect(names.has('bin/yarn')).toBeTruthy();
+    expect(names.has('lib/cli.js')).toBeTruthy();
     expect(names.has('README.md')).toBeFalsy();
   });
 });
