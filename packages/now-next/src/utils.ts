27be8054273ed39f89ab4f0672d856afb11fcc4e
JJ Kasper <jj@jjsweb.site>
2019-06-04 15:41:50 -0700

    Run prettier

--- packages/now-next/src/utils.ts
+++ packages/now-next/src/utils.ts
@@ -6,351 +6,349 @@ import { Files } from '@now/build-utils';
 type stringMap = { [key: string]: string };
 
 export interface EnvConfig {
-	[name: string]: string | undefined;
+  [name: string]: string | undefined;
 }
 
 /**
  * Validate if the entrypoint is allowed to be used
  */
 function validateEntrypoint(entrypoint: string) {
-	if (
-		!/package\.json$/.exec(entrypoint) &&
-		!/next\.config\.js$/.exec(entrypoint)
-	) {
-		throw new Error(
-			'Specified "src" for "@now/next" has to be "package.json" or "next.config.js"'
-		);
-	}
+  if (
+    !/package\.json$/.exec(entrypoint) &&
+    !/next\.config\.js$/.exec(entrypoint)
+  ) {
+    throw new Error(
+      'Specified "src" for "@now/next" has to be "package.json" or "next.config.js"'
+    );
+  }
 }
 
 /**
  * Exclude certain files from the files object
  */
 function excludeFiles(
-	files: Files,
-	matcher: (filePath: string) => boolean
+  files: Files,
+  matcher: (filePath: string) => boolean
 ): Files {
-	return Object.keys(files).reduce((newFiles, filePath) => {
-		if (matcher(filePath)) {
-			return newFiles;
-		}
-		return {
-			...newFiles,
-			[filePath]: files[filePath],
-		};
-	}, {});
+  return Object.keys(files).reduce((newFiles, filePath) => {
+    if (matcher(filePath)) {
+      return newFiles;
+    }
+    return {
+      ...newFiles,
+      [filePath]: files[filePath],
+    };
+  }, {});
 }
 
 /**
  * Creates a new Files object holding only the entrypoint files
  */
 function includeOnlyEntryDirectory(
-	files: Files,
-	entryDirectory: string
+  files: Files,
+  entryDirectory: string
 ): Files {
-	if (entryDirectory === '.') {
-		return files;
-	}
+  if (entryDirectory === '.') {
+    return files;
+  }
 
-	function matcher(filePath: string) {
-		return !filePath.startsWith(entryDirectory);
-	}
+  function matcher(filePath: string) {
+    return !filePath.startsWith(entryDirectory);
+  }
 
-	return excludeFiles(files, matcher);
+  return excludeFiles(files, matcher);
 }
 
 /**
  * Exclude package manager lockfiles from files
  */
 function excludeLockFiles(files: Files): Files {
-	const newFiles = files;
-	if (newFiles['package-lock.json']) {
-		delete newFiles['package-lock.json'];
-	}
-	if (newFiles['yarn.lock']) {
-		delete newFiles['yarn.lock'];
-	}
-	return files;
+  const newFiles = files;
+  if (newFiles['package-lock.json']) {
+    delete newFiles['package-lock.json'];
+  }
+  if (newFiles['yarn.lock']) {
+    delete newFiles['yarn.lock'];
+  }
+  return files;
 }
 
 /**
  * Include only the files from a selected directory
  */
 function filesFromDirectory(files: Files, dir: string): Files {
-	function matcher(filePath: string) {
-		return !filePath.startsWith(dir.replace(/\\/g, '/'));
-	}
+  function matcher(filePath: string) {
+    return !filePath.startsWith(dir.replace(/\\/g, '/'));
+  }
 
-	return excludeFiles(files, matcher);
+  return excludeFiles(files, matcher);
 }
 
 /**
  * Enforce specific package.json configuration for smallest possible lambda
  */
 function normalizePackageJson(
-	defaultPackageJson: {
-		dependencies?: stringMap;
-		devDependencies?: stringMap;
-		scripts?: stringMap;
-	} = {}
+  defaultPackageJson: {
+    dependencies?: stringMap;
+    devDependencies?: stringMap;
+    scripts?: stringMap;
+  } = {}
 ) {
-	const dependencies: stringMap = {};
-	const devDependencies: stringMap = {
-		...defaultPackageJson.dependencies,
-		...defaultPackageJson.devDependencies,
-	};
+  const dependencies: stringMap = {};
+  const devDependencies: stringMap = {
+    ...defaultPackageJson.dependencies,
+    ...defaultPackageJson.devDependencies,
+  };
 
-	if (devDependencies.react) {
-		dependencies.react = devDependencies.react;
-		delete devDependencies.react;
-	}
+  if (devDependencies.react) {
+    dependencies.react = devDependencies.react;
+    delete devDependencies.react;
+  }
 
-	if (devDependencies['react-dom']) {
-		dependencies['react-dom'] = devDependencies['react-dom'];
-		delete devDependencies['react-dom'];
-	}
+  if (devDependencies['react-dom']) {
+    dependencies['react-dom'] = devDependencies['react-dom'];
+    delete devDependencies['react-dom'];
+  }
 
-	return {
-		...defaultPackageJson,
-		dependencies: {
-			// react and react-dom can be overwritten
-			react: 'latest',
-			'react-dom': 'latest',
-			...dependencies, // override react if user provided it
-			// next-server is forced to canary
-			'next-server': 'v7.0.2-canary.49',
-		},
-		devDependencies: {
-			...devDependencies,
-			// next is forced to canary
-			next: 'v7.0.2-canary.49',
-			// next-server is a dependency here
-			'next-server': undefined,
-		},
-		scripts: {
-			...defaultPackageJson.scripts,
-			'now-build':
-				'NODE_OPTIONS=--max_old_space_size=3000 next build --lambdas',
-		},
-	};
+  return {
+    ...defaultPackageJson,
+    dependencies: {
+      // react and react-dom can be overwritten
+      react: 'latest',
+      'react-dom': 'latest',
+      ...dependencies, // override react if user provided it
+      // next-server is forced to canary
+      'next-server': 'v7.0.2-canary.49',
+    },
+    devDependencies: {
+      ...devDependencies,
+      // next is forced to canary
+      next: 'v7.0.2-canary.49',
+      // next-server is a dependency here
+      'next-server': undefined,
+    },
+    scripts: {
+      ...defaultPackageJson.scripts,
+      'now-build':
+        'NODE_OPTIONS=--max_old_space_size=3000 next build --lambdas',
+    },
+  };
 }
 
 async function getNextConfig(workPath: string, entryPath: string) {
-	const entryConfig = path.join(entryPath, './next.config.js');
-	if (await fs.pathExists(entryConfig)) {
-		return fs.readFile(entryConfig, 'utf8');
-	}
+  const entryConfig = path.join(entryPath, './next.config.js');
+  if (await fs.pathExists(entryConfig)) {
+    return fs.readFile(entryConfig, 'utf8');
+  }
 
-	const workConfig = path.join(workPath, './next.config.js');
-	if (await fs.pathExists(workConfig)) {
-		return fs.readFile(workConfig, 'utf8');
-	}
+  const workConfig = path.join(workPath, './next.config.js');
+  if (await fs.pathExists(workConfig)) {
+    return fs.readFile(workConfig, 'utf8');
+  }
 
-	return null;
+  return null;
 }
 
 function pathIsInside(firstPath: string, secondPath: string) {
-	return !path.relative(firstPath, secondPath).startsWith('..');
+  return !path.relative(firstPath, secondPath).startsWith('..');
 }
 
 function getPathsInside(entryDirectory: string, files: Files) {
-	const watch: string[] = [];
+  const watch: string[] = [];
 
-	for (const file of Object.keys(files)) {
-		// If the file is outside of the entrypoint directory, we do
-		// not want to monitor it for changes.
-		if (!pathIsInside(entryDirectory, file)) {
-			continue;
-		}
+  for (const file of Object.keys(files)) {
+    // If the file is outside of the entrypoint directory, we do
+    // not want to monitor it for changes.
+    if (!pathIsInside(entryDirectory, file)) {
+      continue;
+    }
 
-		watch.push(file);
-	}
+    watch.push(file);
+  }
 
-	return watch;
+  return watch;
 }
 
 function normalizePage(page: string): string {
-	// remove '/index' from the end
-	page = page.replace(/\/index$/, '/');
-	// Resolve on anything that doesn't start with `/`
-	if (page[0] !== '/') {
-		page = `/${page}`;
-	}
-	return page;
+  // remove '/index' from the end
+  page = page.replace(/\/index$/, '/');
+  // Resolve on anything that doesn't start with `/`
+  if (page[0] !== '/') {
+    page = `/${page}`;
+  }
+  return page;
 }
 
 function getRoutes(
-	entryPath: string,
-	entryDirectory: string,
-	pathsInside: string[],
-	files: Files,
-	url: string
+  entryPath: string,
+  entryDirectory: string,
+  pathsInside: string[],
+  files: Files,
+  url: string
 ): any[] {
-	const filesInside: Files = {};
-	const prefix = entryDirectory === `.` ? `/` : `/${entryDirectory}/`;
+  const filesInside: Files = {};
+  const prefix = entryDirectory === `.` ? `/` : `/${entryDirectory}/`;
 
-	for (const file of Object.keys(files)) {
-		if (!pathsInside.includes(file)) {
-			continue;
-		}
+  for (const file of Object.keys(files)) {
+    if (!pathsInside.includes(file)) {
+      continue;
+    }
 
-		filesInside[file] = files[file];
-	}
+    filesInside[file] = files[file];
+  }
 
-	const routes: any[] = [
-		{
-			src: `${prefix}_next/(.*)`,
-			dest: `${url}/_next/$1`,
-		},
-		{
-			src: `${prefix}static/(.*)`,
-			dest: `${url}/static/$1`,
-		},
-	];
-	const filePaths = Object.keys(filesInside);
-	const dynamicPages = [];
+  const routes: any[] = [
+    {
+      src: `${prefix}_next/(.*)`,
+      dest: `${url}/_next/$1`,
+    },
+    {
+      src: `${prefix}static/(.*)`,
+      dest: `${url}/static/$1`,
+    },
+  ];
+  const filePaths = Object.keys(filesInside);
+  const dynamicPages = [];
 
-	for (const file of filePaths) {
-		const relativePath = path.relative(entryDirectory, file);
-		const isPage = pathIsInside('pages', relativePath);
+  for (const file of filePaths) {
+    const relativePath = path.relative(entryDirectory, file);
+    const isPage = pathIsInside('pages', relativePath);
 
-		if (!isPage) {
-			continue;
-		}
+    if (!isPage) {
+      continue;
+    }
 
-		const relativeToPages = path.relative('pages', relativePath);
-		const extension = path.extname(relativeToPages);
-		const pageName = relativeToPages.replace(extension, '');
+    const relativeToPages = path.relative('pages', relativePath);
+    const extension = path.extname(relativeToPages);
+    const pageName = relativeToPages.replace(extension, '');
 
-		if (pageName.startsWith('_')) {
-			continue;
-		}
+    if (pageName.startsWith('_')) {
+      continue;
+    }
 
-		if (pageName.startsWith('$') || pageName.includes('/$')) {
-			dynamicPages.push(normalizePage(pageName));
-		}
+    if (pageName.startsWith('$') || pageName.includes('/$')) {
+      dynamicPages.push(normalizePage(pageName));
+    }
 
-		routes.push({
-			src: `${prefix}${pageName}`,
-			dest: `${url}/${pageName}`,
-		});
+    routes.push({
+      src: `${prefix}${pageName}`,
+      dest: `${url}/${pageName}`,
+    });
 
-		if (pageName.endsWith('index')) {
-			const resolvedIndex = pageName
-				.replace('/index', '')
-				.replace('index', '');
+    if (pageName.endsWith('index')) {
+      const resolvedIndex = pageName.replace('/index', '').replace('index', '');
 
-			routes.push({
-				src: `${prefix}${resolvedIndex}`,
-				dest: `${url}/${resolvedIndex}`,
-			});
-		}
-	}
+      routes.push({
+        src: `${prefix}${resolvedIndex}`,
+        dest: `${url}/${resolvedIndex}`,
+      });
+    }
+  }
 
-	routes.push(
-		...getDynamicRoutes(entryPath, entryDirectory, dynamicPages).map(
-			(route: { src: string; dest: string }) => {
-				// convert to make entire RegExp match as one group
-				route.src = route.src.replace('^', '^(').replace('$', ')$');
-				route.dest = `${url}/$1`;
-				return route;
-			}
-		)
-	);
+  routes.push(
+    ...getDynamicRoutes(entryPath, entryDirectory, dynamicPages).map(
+      (route: { src: string; dest: string }) => {
+        // convert to make entire RegExp match as one group
+        route.src = route.src.replace('^', '^(').replace('$', ')$');
+        route.dest = `${url}/$1`;
+        return route;
+      }
+    )
+  );
 
-	// Add public folder routes
-	for (const file of filePaths) {
-		const relativePath = path.relative(entryDirectory, file);
-		const isPublic = pathIsInside('public', relativePath);
+  // Add public folder routes
+  for (const file of filePaths) {
+    const relativePath = path.relative(entryDirectory, file);
+    const isPublic = pathIsInside('public', relativePath);
 
-		if (!isPublic) continue;
+    if (!isPublic) continue;
 
-		const fileName = path.relative('public', relativePath);
-		const route = {
-			src: `${prefix}${fileName}`,
-			dest: `${url}/${fileName}`,
-		};
+    const fileName = path.relative('public', relativePath);
+    const route = {
+      src: `${prefix}${fileName}`,
+      dest: `${url}/${fileName}`,
+    };
 
-		// Only add the route if a page is not already using it
-		if (!routes.some(r => r.src === route.src)) {
-			routes.push(route);
-		}
-	}
+    // Only add the route if a page is not already using it
+    if (!routes.some(r => r.src === route.src)) {
+      routes.push(route);
+    }
+  }
 
-	return routes;
+  return routes;
 }
 
 export function getDynamicRoutes(
-	entryPath: string,
-	entryDirectory: string,
-	dynamicPages: string[]
+  entryPath: string,
+  entryDirectory: string,
+  dynamicPages: string[]
 ): { src: string; dest: string }[] {
-	if (!dynamicPages.length) {
-		return [];
-	}
+  if (!dynamicPages.length) {
+    return [];
+  }
 
-	let getRouteRegex:
-		| ((pageName: string) => { re: RegExp })
-		| undefined = undefined;
+  let getRouteRegex:
+    | ((pageName: string) => { re: RegExp })
+    | undefined = undefined;
 
-	let getSortedRoutes: ((normalizedPages: string[]) => string[]) | undefined;
+  let getSortedRoutes: ((normalizedPages: string[]) => string[]) | undefined;
 
-	try {
-		({ getRouteRegex, getSortedRoutes } = require(resolveFrom(
-			entryPath,
-			'next-server/dist/lib/router/utils'
-		)));
-		if (typeof getRouteRegex !== 'function') {
-			getRouteRegex = undefined;
-		}
-	} catch (_) {}
+  try {
+    ({ getRouteRegex, getSortedRoutes } = require(resolveFrom(
+      entryPath,
+      'next-server/dist/lib/router/utils'
+    )));
+    if (typeof getRouteRegex !== 'function') {
+      getRouteRegex = undefined;
+    }
+  } catch (_) {}
 
-	if (!getRouteRegex || !getSortedRoutes) {
-		throw new Error(
-			'Found usage of dynamic routes but not on a new enough version of Next.js.'
-		);
-	}
+  if (!getRouteRegex || !getSortedRoutes) {
+    throw new Error(
+      'Found usage of dynamic routes but not on a new enough version of Next.js.'
+    );
+  }
 
-	const pageMatchers = getSortedRoutes(dynamicPages).map(pageName => ({
-		pageName,
-		matcher: getRouteRegex!(pageName).re,
-	}));
+  const pageMatchers = getSortedRoutes(dynamicPages).map(pageName => ({
+    pageName,
+    matcher: getRouteRegex!(pageName).re,
+  }));
 
-	const routes: { src: string; dest: string }[] = [];
-	pageMatchers.forEach(pageMatcher => {
-		routes.push({
-			src: pageMatcher.matcher.source,
-			dest: path.join('/', entryDirectory, pageMatcher.pageName),
-		});
-	});
-	return routes;
+  const routes: { src: string; dest: string }[] = [];
+  pageMatchers.forEach(pageMatcher => {
+    routes.push({
+      src: pageMatcher.matcher.source,
+      dest: path.join('/', entryDirectory, pageMatcher.pageName),
+    });
+  });
+  return routes;
 }
 
 function syncEnvVars(base: EnvConfig, removeEnv: EnvConfig, addEnv: EnvConfig) {
-	// Remove any env vars from `removeEnv`
-	// that are not present in the `addEnv`
-	const addKeys = new Set(Object.keys(addEnv));
-	for (const name of Object.keys(removeEnv)) {
-		if (!addKeys.has(name)) {
-			delete base[name];
-		}
-	}
+  // Remove any env vars from `removeEnv`
+  // that are not present in the `addEnv`
+  const addKeys = new Set(Object.keys(addEnv));
+  for (const name of Object.keys(removeEnv)) {
+    if (!addKeys.has(name)) {
+      delete base[name];
+    }
+  }
 
-	// Add in the keys from `addEnv`
-	Object.assign(base, addEnv);
+  // Add in the keys from `addEnv`
+  Object.assign(base, addEnv);
 }
 
 export {
-	excludeFiles,
-	validateEntrypoint,
-	includeOnlyEntryDirectory,
-	excludeLockFiles,
-	normalizePackageJson,
-	filesFromDirectory,
-	getNextConfig,
-	getPathsInside,
-	getRoutes,
-	stringMap,
-	syncEnvVars,
-	normalizePage,
+  excludeFiles,
+  validateEntrypoint,
+  includeOnlyEntryDirectory,
+  excludeLockFiles,
+  normalizePackageJson,
+  filesFromDirectory,
+  getNextConfig,
+  getPathsInside,
+  getRoutes,
+  stringMap,
+  syncEnvVars,
+  normalizePage,
 };
