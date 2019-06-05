acdc164ea73a20def3dfaf2341acbd2182e15389
JJ Kasper <jj@jjsweb.site>
2019-06-05 16:19:09 -0700

    Update to use template literals

--- packages/now-next/src/index.ts
+++ packages/now-next/src/index.ts
@@ -510,8 +510,8 @@ export const build = async ({
   ).map(route => {
     // make sure .html is added to dest for now until
     // outputting static files to clean routes is available
-    if (staticPages[route.dest + '.html']) {
-      route.dest += '.html';
+    if (staticPages[`${route.dest}.html`]) {
+      route.dest = `${route.dest}.html`;
     }
     return route;
   });
