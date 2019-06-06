0035e67f29913aa1b3121c6d8fc3d72e0133f25a
Luc <luc.leray@gmail.com>
2019-06-07 00:24:14 +0200

    Update packages/now-node/test/fixtures/15-helpers/no-helpers/index.js
    Co-Authored-By: Steven <steven@ceriously.com>

--- packages/now-node/test/fixtures/15-helpers/no-helpers/index.js
+++ packages/now-node/test/fixtures/15-helpers/no-helpers/index.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-  const areHelpersAvailable = !!req.query;
+  const areHelpersAvailable = typeof req.query !== 'undefined';
 
   res.end(`${areHelpersAvailable ? 'yes' : 'no'}:RANDOMNESS_PLACEHOLDER`);
 };
