fe6f4970c915d36005c34acc0ffd7012ec45a22f
Nathan Cahill <nathan@nathancahill.com>
2019-06-07 20:11:09 -0400

    fix linting errors

--- packages/now-ruby/index.ts
+++ packages/now-ruby/index.ts
@@ -1,204 +1,212 @@
 import { join, dirname } from 'path';
 import execa from 'execa';
 import {
-	ensureDir,
-	move,
-	remove,
-	pathExists,
-	readFile,
-	writeFile,
+  ensureDir,
+  move,
+  remove,
+  pathExists,
+  readFile,
+  writeFile,
 } from 'fs-extra';
 import {
-	download,
-	getWriteableDirectory,
-	glob,
-	createLambda,
-	BuildOptions,
+  download,
+  getWriteableDirectory,
+  glob,
+  createLambda,
+  BuildOptions,
 } from '@now/build-utils';
 import { installBundler } from './install-ruby';
+
+interface RubyConfigOptions {
+  [key: string]: string;
+}
+
+interface RubyBuildOptions extends BuildOptions {
+  config: RubyConfigOptions;
+}
 
 const REQUIRED_VENDOR_DIR = 'vendor/bundle/ruby/2.5.0';
 
 async function bundleInstall(
-	bundlePath: string,
-	bundleDir: string,
-	gemfilePath: string
+  bundlePath: string,
+  bundleDir: string,
+  gemfilePath: string
 ) {
-	console.log(`running "bundle install --deployment"...`);
-	const bundleAppConfig = await getWriteableDirectory();
+  console.log(`running "bundle install --deployment"...`);
+  const bundleAppConfig = await getWriteableDirectory();
 
-	try {
-		await execa(
-			bundlePath,
-			[
-				'install',
-				'--deployment',
-				'--gemfile',
-				gemfilePath,
-				'--path',
-				bundleDir,
-			],
-			{
-				stdio: 'inherit',
-				env: {
-					BUNDLE_SILENCE_ROOT_WARNING: '1',
-					BUNDLE_APP_CONFIG: bundleAppConfig,
-				},
-			}
-		);
-	} catch (err) {
-		console.log(`failed to run "bundle install --deployment"...`);
-		throw err;
-	}
+  try {
+    await execa(
+      bundlePath,
+      [
+        'install',
+        '--deployment',
+        '--gemfile',
+        gemfilePath,
+        '--path',
+        bundleDir,
+      ],
+      {
+        stdio: 'inherit',
+        env: {
+          BUNDLE_SILENCE_ROOT_WARNING: '1',
+          BUNDLE_APP_CONFIG: bundleAppConfig,
+        },
+      }
+    );
+  } catch (err) {
+    console.log(`failed to run "bundle install --deployment"...`);
+    throw err;
+  }
 }
 
 export const config = {
-	maxLambdaSize: '5mb',
+  maxLambdaSize: '5mb',
 };
 
 export const build = async ({
-	workPath,
-	files,
-	entrypoint,
-	config,
-}: BuildOptions) => {
-	console.log('downloading files...');
+  workPath,
+  files,
+  entrypoint,
+  config,
+}: RubyBuildOptions) => {
+  console.log('downloading files...');
 
-	// eslint-disable-next-line no-param-reassign
-	files = await download(files, workPath);
+  // eslint-disable-next-line no-param-reassign
+  files = await download(files, workPath);
 
-	const { gemHome, bundlerPath } = await installBundler();
-	process.env.GEM_HOME = gemHome;
+  const { gemHome, bundlerPath } = await installBundler();
+  process.env.GEM_HOME = gemHome;
 
-	const fsFiles = await glob('**', workPath);
-	const entryDirectory = dirname(entrypoint);
-	const fsEntryDirectory = dirname(fsFiles[entrypoint].fsPath);
+  const fsFiles = await glob('**', workPath);
+  const entryDirectory = dirname(entrypoint);
+  const fsEntryDirectory = dirname(fsFiles[entrypoint].fsPath);
 
-	// check for an existing vendor directory
-	console.log(
-		'checking for existing vendor directory at',
-		'"' + REQUIRED_VENDOR_DIR + '"'
-	);
-	const vendorDir = join(workPath, REQUIRED_VENDOR_DIR);
-	const bundleDir = join(workPath, 'vendor/bundle');
-	const relativeVendorDir = join(fsEntryDirectory, REQUIRED_VENDOR_DIR);
+  // check for an existing vendor directory
+  console.log(
+    'checking for existing vendor directory at',
+    '"' + REQUIRED_VENDOR_DIR + '"'
+  );
+  const vendorDir = join(workPath, REQUIRED_VENDOR_DIR);
+  const bundleDir = join(workPath, 'vendor/bundle');
+  const relativeVendorDir = join(fsEntryDirectory, REQUIRED_VENDOR_DIR);
 
-	let hasRootVendorDir = await pathExists(vendorDir);
-	let hasRelativeVendorDir = await pathExists(relativeVendorDir);
-	let hasVendorDir = hasRootVendorDir || hasRelativeVendorDir;
+  let hasRootVendorDir = await pathExists(vendorDir);
+  let hasRelativeVendorDir = await pathExists(relativeVendorDir);
+  let hasVendorDir = hasRootVendorDir || hasRelativeVendorDir;
 
-	if (hasRelativeVendorDir) {
-		if (hasRootVendorDir) {
-			console.log(
-				'found two vendor directories, choosing the vendor directory relative to entrypoint'
-			);
-		} else {
-			console.log('found vendor directory relative to entrypoint');
-		}
+  if (hasRelativeVendorDir) {
+    if (hasRootVendorDir) {
+      console.log(
+        'found two vendor directories, choosing the vendor directory relative to entrypoint'
+      );
+    } else {
+      console.log('found vendor directory relative to entrypoint');
+    }
 
-		// vendor dir must be at the root for lambda to find it
-		await move(relativeVendorDir, vendorDir);
-	} else if (hasRootVendorDir) {
-		console.log('found vendor directory in project root');
-	}
+    // vendor dir must be at the root for lambda to find it
+    await move(relativeVendorDir, vendorDir);
+  } else if (hasRootVendorDir) {
+    console.log('found vendor directory in project root');
+  }
 
-	await ensureDir(vendorDir);
+  await ensureDir(vendorDir);
 
-	// no vendor directory, check for Gemfile to install
-	if (!hasVendorDir) {
-		const gemFile = join(entryDirectory, 'Gemfile');
+  // no vendor directory, check for Gemfile to install
+  if (!hasVendorDir) {
+    const gemFile = join(entryDirectory, 'Gemfile');
 
-		if (fsFiles[gemFile]) {
-			console.log(
-				'did not find a vendor directory but found a Gemfile, bundling gems...'
-			);
-			const gemfilePath = fsFiles[gemFile].fsPath;
+    if (fsFiles[gemFile]) {
+      console.log(
+        'did not find a vendor directory but found a Gemfile, bundling gems...'
+      );
+      const gemfilePath = fsFiles[gemFile].fsPath;
 
-			// try installing. this won't work if native extesions are required.
-			// if that's the case, gems should be vendored locally before deploying.
-			try {
-				await bundleInstall(bundlerPath, bundleDir, gemfilePath);
-			} catch (err) {
-				console.log(
-					'unable to build gems from Gemfile. vendor the gems locally with "bundle install --deployment" and retry.'
-				);
-				throw err;
-			}
-		}
-	} else {
-		console.log('found vendor directory, skipping "bundle install"...');
-	}
+      // try installing. this won't work if native extesions are required.
+      // if that's the case, gems should be vendored locally before deploying.
+      try {
+        await bundleInstall(bundlerPath, bundleDir, gemfilePath);
+      } catch (err) {
+        console.log(
+          'unable to build gems from Gemfile. vendor the gems locally with "bundle install --deployment" and retry.'
+        );
+        throw err;
+      }
+    }
+  } else {
+    console.log('found vendor directory, skipping "bundle install"...');
+  }
 
-	// try to remove gem cache to slim bundle size
-	try {
-		await remove(join(vendorDir, 'cache'));
-	} catch (e) {}
+  // try to remove gem cache to slim bundle size
+  try {
+    await remove(join(vendorDir, 'cache'));
+  } catch (e) {}
 
-	const originalRbPath = join(__dirname, '..', 'now_init.rb');
-	const originalNowHandlerRbContents = await readFile(originalRbPath, 'utf8');
+  const originalRbPath = join(__dirname, '..', 'now_init.rb');
+  const originalNowHandlerRbContents = await readFile(originalRbPath, 'utf8');
 
-	// will be used on `require_relative '$here'` or for loading rack config.ru file
-	// for example, `require_relative 'api/users'`
-	console.log('entrypoint is', entrypoint);
-	const userHandlerFilePath = entrypoint.replace(/\.rb$/, '');
-	const nowHandlerRbContents = originalNowHandlerRbContents.replace(
-		/__NOW_HANDLER_FILENAME/g,
-		userHandlerFilePath
-	);
+  // will be used on `require_relative '$here'` or for loading rack config.ru file
+  // for example, `require_relative 'api/users'`
+  console.log('entrypoint is', entrypoint);
+  const userHandlerFilePath = entrypoint.replace(/\.rb$/, '');
+  const nowHandlerRbContents = originalNowHandlerRbContents.replace(
+    /__NOW_HANDLER_FILENAME/g,
+    userHandlerFilePath
+  );
 
-	// in order to allow the user to have `server.rb`, we need our `server.rb` to be called
-	// somethig else
-	const nowHandlerRbFilename = 'now__handler__ruby';
+  // in order to allow the user to have `server.rb`, we need our `server.rb` to be called
+  // somethig else
+  const nowHandlerRbFilename = 'now__handler__ruby';
 
-	await writeFile(
-		join(workPath, `${nowHandlerRbFilename}.rb`),
-		nowHandlerRbContents
-	);
+  await writeFile(
+    join(workPath, `${nowHandlerRbFilename}.rb`),
+    nowHandlerRbContents
+  );
 
-	let outputFiles = await glob('**', workPath);
+  let outputFiles = await glob('**', workPath);
 
-	// static analysis is impossible with ruby.
-	// instead, provide `includeFiles` and `excludeFiles` config options to reduce bundle size.
-	if (config && (config.includeFiles || config.excludeFiles)) {
-		const included = config.includeFiles
-			? await glob(config.includeFiles, workPath)
-			: outputFiles;
+  // static analysis is impossible with ruby.
+  // instead, provide `includeFiles` and `excludeFiles` config options to reduce bundle size.
+  if (config && (config.includeFiles || config.excludeFiles)) {
+    const included = config.includeFiles
+      ? await glob(config.includeFiles, workPath)
+      : outputFiles;
 
-		const excluded = config.excludeFiles
-			? await glob(config.excludeFiles, workPath)
-			: {};
+    const excluded = config.excludeFiles
+      ? await glob(config.excludeFiles, workPath)
+      : {};
 
-		const includedPaths = Object.keys(included);
-		const excludedPaths = Object.keys(excluded);
+    const includedPaths = Object.keys(included);
+    const excludedPaths = Object.keys(excluded);
 
-		for (let i = 0; i < excludedPaths.length; i++) {
-			// whitelist includeFiles
-			if (includedPaths.includes(excludedPaths[i])) {
-				continue;
-			}
+    for (let i = 0; i < excludedPaths.length; i++) {
+      // whitelist includeFiles
+      if (includedPaths.includes(excludedPaths[i])) {
+        continue;
+      }
 
-			// whitelist handler
-			if (excludedPaths[i] === `${nowHandlerRbFilename}.rb`) {
-				continue;
-			}
+      // whitelist handler
+      if (excludedPaths[i] === `${nowHandlerRbFilename}.rb`) {
+        continue;
+      }
 
-			// whitelist vendor directory
-			if (excludedPaths[i].startsWith(REQUIRED_VENDOR_DIR)) {
-				continue;
-			}
+      // whitelist vendor directory
+      if (excludedPaths[i].startsWith(REQUIRED_VENDOR_DIR)) {
+        continue;
+      }
 
-			delete outputFiles[excludedPaths[i]];
-		}
-	}
+      delete outputFiles[excludedPaths[i]];
+    }
+  }
 
-	const lambda = await createLambda({
-		files: outputFiles,
-		handler: `${nowHandlerRbFilename}.now__handler`,
-		runtime: 'ruby2.5',
-		environment: {},
-	});
+  const lambda = await createLambda({
+    files: outputFiles,
+    handler: `${nowHandlerRbFilename}.now__handler`,
+    runtime: 'ruby2.5',
+    environment: {},
+  });
 
-	return {
-		[entrypoint]: lambda,
-	};
+  return {
+    [entrypoint]: lambda,
+  };
 };
