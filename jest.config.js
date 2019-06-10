07debe05b6685a70919543e270047f82bf9a9379
luc <luc.leray@gmail.com>
2019-06-10 14:49:42 +0200

    add ts-jest

--- jest.config.js
+++ jest.config.js
@@ -31,6 +31,7 @@ const testMatch = Array.from(new Set(matches)).map(
 
 module.exports = {
   testEnvironment: 'node',
+  preset: 'ts-jest', // only applies to .ts files
   testMatch,
   collectCoverageFrom: [
     'packages/(!test)/**/*.{js,jsx}',
