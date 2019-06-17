f3118819c90ebe1a1a75564aeaf45c87c89078c1
Luc <luc.leray@gmail.com>
2019-06-17 21:32:46 +0200

    add `now` in file names
    Co-Authored-By: Steven <steven@ceriously.com>

--- packages/now-node/src/index.ts
+++ packages/now-node/src/index.ts
@@ -31,9 +31,9 @@ interface DownloadOptions {
 
 const watchers: Map<string, NccWatcher> = new Map();
 
-const LAUNCHER_FILENAME = '___launch3r';
-const BRIDGE_FILENAME = '___bridg3';
-const HELPERS_FILENAME = '___help3rs';
+const LAUNCHER_FILENAME = '___now_launch3r';
+const BRIDGE_FILENAME = '___now_bridg3';
+const HELPERS_FILENAME = '___now_help3rs';
 
 function getWatcher(entrypoint: string, options: NccOptions): NccWatcher {
   let watcher = watchers.get(entrypoint);
