27e015997611066ee223544ce618c2b2895785b9
Steven <steven@ceriously.com>
2019-06-24 12:15:40 -0400

    [now-build-utils] Fail if engines does not match

--- packages/now-build-utils/src/fs/node-version.ts
+++ packages/now-build-utils/src/fs/node-version.ts
@@ -3,11 +3,12 @@ import { NodeVersion } from '../types';
 
 const supportedOptions: NodeVersion[] = [
   { major: 10, range: '10.x', runtime: 'nodejs10.x' },
-  { major: 8, range: '8.x', runtime: 'nodejs8.10' },
+  { major: 8, range: '8.10.x', runtime: 'nodejs8.10' },
 ];
 
 // This version should match Fargate's default in the PATH
-export const defaultSelection = supportedOptions[supportedOptions.length - 1];
+// Today that is Node 8
+export const defaultSelection = supportedOptions.filter(o => o.major === 8)[0];
 
 export async function getSupportedNodeVersion(
   engineRange?: string
@@ -16,24 +17,28 @@ export async function getSupportedNodeVersion(
 
   if (!engineRange) {
     console.log(
-      'missing `engines` in `package.json`, using default node v' +
-        selection.major
+      'missing `engines` in `package.json`, using default runtime' +
+        selection.runtime
     );
   } else {
-    for (let o of supportedOptions) {
-      if (intersects(o.range, engineRange)) {
-        selection = o;
-        console.log(
-          'found `engines` in `package.json`, selecting node v' +
-            selection.major
-        );
-        return selection; // the array is ordered so break early to use the first (largest) match
-      }
+    const found = supportedOptions.some(o => {
+      // the array is already in order so return the first
+      // match which will be the newest verison of node
+      selection = o;
+      return intersects(o.range, engineRange);
+    });
+    if (found) {
+      console.log(
+        'found `engines` in `package.json`, selecting runtime' +
+          selection.runtime
+      );
+    } else {
+      throw new Error(
+        'found `engines` in `package.json` with an unsupported node range: ' +
+          engineRange +
+          '\nplease use `10.x` or `8.10.x` instead'
+      );
     }
-    console.log(
-      'WARNING: version of `engines` in `package.json` is not supported, using default node v' +
-        selection.major
-    );
   }
   return selection;
 }
