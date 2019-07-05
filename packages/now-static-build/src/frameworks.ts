f155b8f4a8b3f21f47ea1a30c3e78a97c92e915c
Leo Lamprecht <mindrun@icloud.com>
2019-07-05 10:46:57 +0000

    Added Aurelia

--- packages/now-static-build/src/frameworks.ts
+++ packages/now-static-build/src/frameworks.ts
@@ -135,4 +135,18 @@ export default [
       },
     ],
   },
+  {
+    name: 'Aurelia',
+    dependency: 'aurelia-cli',
+    getOutputDirName: async () => 'dist',
+    defaultRoutes: [
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '/(.*)',
+        dest: '/index.html',
+      },
+    ],
+  },
 ];
