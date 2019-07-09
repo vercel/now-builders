f18a397a2f71ed00a1d0d5cc4b77844d4e21c74a
Steven <steven@ceriously.com>
2019-07-09 13:35:47 -0400

    Add monkey image in test

--- packages/now-node/test/fixtures/30-sharp/index.js
+++ packages/now-node/test/fixtures/30-sharp/index.js
@@ -1,21 +1,14 @@
-const download = require('download');
-const { tmpdir } = require('os');
+const { readFileSync } = require('fs');
 const { join } = require('path');
 const sharp = require('sharp');
 
-const url = 'https://icdn2.digitaltrends.com/image/monkey-selfie-david-slater.jpg';
-const tmp = tmpdir();
-
 module.exports = async (req, res) => {
-  const file = join(tmp, 'monkey-selfie-david-slater.jpg');
-  console.log(`incoming req, downloading image to ${file}`);
-  await download(url, tmp);
-  console.log('downloaded. resizing image...');
+  const file = readFileSync(join(__dirname, 'monkey.jpg'));
+  console.log(`resizing image ${file}`);
   const image = sharp(file).resize({
     height: 100,
     width: 100,
   });
-  res.writeHead(200, { 'Content-Type': 'image/jpeg' });
   const buffer = await image.jpeg().toBuffer();
   if (buffer.length > 0) {
     res.end('sharp:RANDOMNESS_PLACEHOLDER');
