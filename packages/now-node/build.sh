5d2acceccbf9669b78c393a18e33e4d99a37eb20
luc <luc.leray@gmail.com>
2019-07-17 01:33:21 +0200

    do not copy bridge.d.ts over

--- packages/now-node/build.sh
+++ packages/now-node/build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-bridge_entrypoint="$(node -p 'require.resolve("@now/node-bridge")')"
-bridge_defs="$(dirname "$bridge_entrypoint")/bridge.d.ts"
-
-if [ ! -e "$bridge_defs" ]; then
-  yarn install
-fi
-
-cp -v "$bridge_defs" src
-
 # build ts files
 tsc
 
