351c84c7bea1313742c165f753e7481fc7c3c0fa
Andy Bitz <artzbitz@gmail.com>
2019-07-19 11:24:48 +0200

    Fix test

--- packages/now-static-build/test/fixtures/02-cowsay-sh/some-build.sh
+++ packages/now-static-build/test/fixtures/02-cowsay-sh/some-build.sh
@@ -1,3 +1,3 @@
 yarn install
-mkdir dist
-./node_modules/.bin/cowsay cow:RANDOMNESS_PLACEHOLDER > dist/index.txt
+mkdir public
+./node_modules/.bin/cowsay cow:RANDOMNESS_PLACEHOLDER > public/index.txt
