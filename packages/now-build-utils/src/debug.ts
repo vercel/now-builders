f8edc67c25cfd7892172007e0e0a3c27247a67d3
Sophearak Tha <t.sophearak@gmail.com>
2019-07-26 21:43:29 +0700

    Apply suggestions from code review
    Co-Authored-By: Steven <steven@ceriously.com>

--- packages/now-build-utils/src/debug.ts
+++ packages/now-build-utils/src/debug.ts
@@ -1,7 +1,7 @@
 export default function debug(message: string, additional?: object | string[]) {
-  if (process.env.ENABLE_DEBUG_LOGS) {
+  if (process.env.NOW_BUILDER_DEBUG) {
     console.log(message, additional);
-  } else if (process.env.ANNOTATE_DEBUG_LOGS) {
-    console.log(`[builder-debug-log-line] ${message}`, additional);
+  } else if (process.env.NOW_BUILDER_ANNOTATE) {
+    console.log(`[now-builder-debug] ${message}`);
   }
 }
