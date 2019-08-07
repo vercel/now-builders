1bb581e893ed494af070736c8aa4291ed7227f07
Joe Haddad <joe.haddad@zeit.co>
2019-08-06 21:39:01 -0400

    Add firebase fixture

--- packages/now-next/src/create-serverless-config.ts
+++ packages/now-next/src/create-serverless-config.ts
@@ -50,7 +50,7 @@ export default async function createServerlessConfig(
       if (semver.satisfies(nextVersion, `>=${ExperimentalTraceVersion}`)) {
         target = 'experimental-serverless-trace';
       }
-    } catch {}
+    } catch (_ignored) {}
   }
 
   const configPath = path.join(workPath, 'next.config.js');
