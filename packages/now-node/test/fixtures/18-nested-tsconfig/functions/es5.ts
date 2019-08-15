3544799d1512d8de86bd9d6ce49e7a618f9d6af9
Steven <steven@ceriously.com>
2019-08-15 12:27:52 -0700

    Fix tests

--- packages/now-node/test/fixtures/18-nested-tsconfig/functions/es5.ts
+++ packages/now-node/test/fixtures/18-nested-tsconfig/functions/es5.ts
@@ -1,12 +1,9 @@
 import { IncomingMessage, ServerResponse } from 'http';
-import { readFileSync } from 'fs';
 import { hello } from '../dep';
 
 export default function(req: IncomingMessage, res: ServerResponse) {
-  hello('world');
-  const str = readFileSync('../dep.js', 'utf8');
-  if (req && str) {
-    res.end(str);
+  if (req) {
+    res.end(hello.toString());
   } else {
     res.end('no req found');
   }
--- packages/now-node/test/fixtures/18-nested-tsconfig/index.ts
+++ packages/now-node/test/fixtures/18-nested-tsconfig/index.ts
@@ -1,12 +1,9 @@
 import { NowRequest, NowResponse } from '@now/node';
-import { readFileSync } from 'fs';
 import { hello } from './dep';
 
 export default function(req: NowRequest, res: NowResponse) {
-  hello('world');
-  const str = readFileSync('./dep.js', 'utf8');
-  if (req && str) {
-    res.end(str);
+  if (req) {
+    res.end(hello.toString());
   } else {
     res.end('no req found');
   }
